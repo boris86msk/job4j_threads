4791cf2a712b1899358721b1595fbb41b844c48c
Boris <boris86msk@yandex.ru>
2023-06-28 11:21:33 +0300

    5. Прерывание нити [#1019 #426015]

---
+++ src/main/java/ru/job4j/concurrent/ConsoleProgress.java
@@ -0,0 +1,28 @@
+package ru.job4j.concurrent;
+
+public class ConsoleProgress implements Runnable {
+    public static void main(String[] args) throws InterruptedException {
+        Thread progress = new Thread(new ConsoleProgress());
+        progress.start();
+        Thread.sleep(5000);
+        progress.interrupt();
+    }
+
+    @Override
+    public void run() {
+        char[] process = new char[]{'-', '\\', '|', '/'};
+        int count = 0;
+        try {
+            while (!Thread.currentThread().isInterrupted()) {
+                Thread.sleep(500);
+                System.out.print("\r load: " + process[count]);
+                count++;
+                if (count == process.length) {
+                    count = 0;
+                }
+            }
+        } catch (InterruptedException e) {
+            e.printStackTrace();
+        }
+    }
+}
