3cc62f3cbced1671bed567e5f85426f08d09950c
Boris <boris86msk@yandex.ru>
2023-06-28 21:43:09 +0300

    6. Прерывание блокированной нити

--- src/main/java/ru/job4j/concurrent/ConsoleProgress.java
+++ src/main/java/ru/job4j/concurrent/ConsoleProgress.java
@@ -22,7 +22,7 @@ public class ConsoleProgress implements Runnable {
                 }
             }
         } catch (InterruptedException e) {
-            e.printStackTrace();
+            Thread.currentThread().interrupt();
         }
     }
 }
