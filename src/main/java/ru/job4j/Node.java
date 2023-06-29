41e7046716bfa37c4785aefb3a961150fb714752
Boris <boris86msk@yandex.ru>
2023-06-29 20:56:21 +0300

    Immutable объекты 2

--- src/main/java/ru/job4j/Node.java
+++ src/main/java/ru/job4j/Node.java
@@ -1,26 +1,15 @@
 package ru.job4j;
 
-public class Node<T> {
-    private volatile Node<T> next;
-    private volatile T value;
+public final class Node<T> {
+    private Node<T> next;
+    private T value;
 
     public Node<T> getNext() {
         return next;
-    }
-
-    public void setNext(Node<T> next) {
-        if (this.next == null) {
-            this.next = next;
-        }
     }
 
     public T getValue() {
         return value;
     }
 
-    public void setValue(T value) {
-        if (this.value == null) {
-            this.value = value;
-        }
-    }
 }
