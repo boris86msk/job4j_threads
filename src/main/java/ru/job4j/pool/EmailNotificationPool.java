adc85faf71ce5219abdfb77769a3f99ecca896e8
boris86msk <boris86msk@yandex.ru>
2023-07-19 22:54:36 +0300

    2. рассылка почты public -> private

--- src/main/java/ru/job4j/pool/EmailNotificationPool.java
+++ src/main/java/ru/job4j/pool/EmailNotificationPool.java
@@ -4,7 +4,7 @@ import java.util.concurrent.ExecutorService;
 import java.util.concurrent.Executors;
 
 public class EmailNotificationPool {
-    public ExecutorService pool = Executors.newFixedThreadPool(
+    private final ExecutorService pool = Executors.newFixedThreadPool(
             Runtime.getRuntime().availableProcessors());
 
     public void emailTo(UserPool user) {
